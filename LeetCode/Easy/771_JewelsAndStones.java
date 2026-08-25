4d96c815e774026882221968a93bea8491280e7b
guptanikhil4763-arch <guptanikhil4763@gmail.com>
2026-08-25 22:27:39 +0530

    Sync LeetCode - 771. Jewels and Stones (Easy)

---
+++ LeetCode/Easy/771_JewelsAndStones.java
@@ -0,0 +1,19 @@
+/**
+ * Problem Link : https://leetcode.com/problems/jewels-and-stones/
+ * Platform     : LeetCode
+ * Difficulty   : Easy
+ */
+
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count =0;
+        for( int i =0; i<stones.length(); i++){
+            for(  int j =0; j<jewels.length(); j++){
+                if(stones.charAt(i)==jewels.charAt(j){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
