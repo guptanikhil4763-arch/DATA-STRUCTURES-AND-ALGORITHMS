c0bfee36622db19ba3ec1dd25b603099c4de3308
guptanikhil4763-arch <guptanikhil4763@gmail.com>
2026-09-01 22:03:27 +0530

    Sync GFG - Capitalize First Letter of Words (Basic)

--- GFG/Basic/0_CapitalizeFirstLetterOfWords.java
+++ GFG/Basic/0_CapitalizeFirstLetterOfWords.java
@@ -7,18 +7,15 @@
 class Solution {
     public String convert(String s) {
         // code here
-      String[] arr = s.split(" ");
-      for( int i =0; i<arr.length; i++){
-          String str =arr[i];
-          for( int j =0; j<str.length(); j++){
-              String str1 = str.substring(0,1).toUpperCase();
-              String str2 = str.substring(1,str.length());
-              str = str1+str2;
-          }
-          arr[i] =str;
-      }
-      String result = String.join(" ", arr);
-      return result;
-       
+        String[] arr = s.split(" ");
+        for( int i =0; i<arr.length; i++){
+            String str= arr[i];
+            String str1 =str.substring(0,1).toUpperCase();
+            String str2 =str.substring(1,str.length());
+            str = str1+str2;
+            arr[i] = str;
+        }
+        String result =  String.join(" ",arr);
+        return result;
     }
 };
