dd43dd6c004100c9fe2896ff87fb36c5711f4bf5
guptanikhil4763-arch <guptanikhil4763@gmail.com>
2026-09-02 12:45:32 +0530

    Sync GFG - C++ Strings (Basic)

---
+++ GFG/Basic/0_CStrings.java
@@ -0,0 +1,13 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/c-strings4609/1
+ * Platform     : GFG
+ * Difficulty   : Basic
+ */
+
+class Sol {
+    String conCat(String s1, String s2) {
+        // code here.
+        String s3= s1+s2;
+        return s3;
+    }
+}
