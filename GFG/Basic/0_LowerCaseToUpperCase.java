c94731e981354bd73118580cb2864036684ae490
guptanikhil4763-arch <guptanikhil4763@gmail.com>
2026-09-02 12:44:40 +0530

    Sync GFG - Lower case to upper case (Basic)

---
+++ GFG/Basic/0_LowerCaseToUpperCase.java
@@ -0,0 +1,14 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/lower-case-to-upper-case3410/1
+ * Platform     : GFG
+ * Difficulty   : Basic
+ */
+
+class Solution {
+    String to_upper(String str) {
+        // code here
+        String s = str.toUpperCase();
+        return s;
+        
+    }
+}
