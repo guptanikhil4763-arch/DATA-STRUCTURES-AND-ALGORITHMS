90eaabda6fa9536484a05a2bfe4e590293f25c62
guptanikhil4763-arch <guptanikhil4763@gmail.com>
2026-09-02 16:00:45 +0530

    Sync GFG - Count Words in a String (Easy)

---
+++ GFG/Easy/0_CountWordsInAString.java
@@ -0,0 +1,36 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/count-number-of-words1500/1
+ * Platform     : GFG
+ * Difficulty   : Easy
+ */
+
+class Solution {
+    public int countWords(String s) {
+
+        ArrayList<String> list = new ArrayList<>();
+
+        int j = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == ' ' ||
+                s.charAt(i) == '\n' ||
+                s.charAt(i) == '\t') {
+
+                if (j < i) {
+                    String str = s.substring(j, i);
+                    list.add(str);
+                }
+
+                j = i + 1;
+            }
+        }
+
+        if (j < s.length()) {
+            String str = s.substring(j, s.length());
+            list.add(str);
+        }
+
+        return list.size();
+    }
+}
