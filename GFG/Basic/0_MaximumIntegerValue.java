159052d0440649b381fd95c4030efa81fa8ffcf8
Nikhil Gupta <guptanikhil4763@gmail.com>
2026-09-05 04:31:35 +0530

    Sync GFG - Maximum Integer Value (Basic)

---
+++ GFG/Basic/0_MaximumIntegerValue.java
@@ -0,0 +1,24 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/maximum-integer-value1434/1
+ * Platform     : GFG
+ * Difficulty   : Basic
+ */
+
+class Solution {
+    public int maximumIntegerValue(String s) {
+
+     int ans = 0;
+     for(int i = 0;i<s.length();i++){
+         int a = s.charAt(i)-'0';
+         if(a<=1 || ans<=1){
+             ans = ans+a;
+         }
+         else{
+             ans = ans*a;
+         }
+     }
+     return ans;
+      
+      
+    }
+}
